ea0ee85f26485208cd60337c2ef973496402ecde
Liam Ellis <liamhellis@gmail.com>
2016-04-08 16:37:00 -0700

    add a README file and the finalization of some UI logic

---
+++ README.md
@@ -0,0 +1,34 @@
+# _{Pizza Project}_
+
+#### _{Pizza Website using Javascript Objects}, {April 8th 2016}_
+
+#### By _**{Liam Ellis}**_
+
+## Description
+
+_{Pizza website. Custom styled using some Bootstrap for the form and inputs. Backend is written in Javascript and Jquery. the site uses Javascript objects to create totals for pizza orders based on toppings and side}_
+
+## Setup/Installation Requirements
+
+- Access to the internet
+- Modern Web Browser
+
+
+## Known Bugs
+
+_{There are no known bugs. Please contact me if you find this to be otherwise}_
+
+## Support and contact details
+
+Liam Ellis
+
+612-239-8433
+
+## Technologies Used
+
+_{HTML, CSS, Javascript}_
+
+### License
+
+
+Copyright (c) 2016 **_{Liam Ellis & Bootstrap}_**
--- js/main.js
+++ js/main.js
@@ -15,6 +15,10 @@ function Order(){
 	this.total = [];
 }
 
+Pizza.prototype.orderTotal = function(price1){
+	return "Thanks for your order! Your total comes to $" + (price1 + this.price);
+};
+
 Order.prototype.addPrice = function(){
 	var price = 0;
 	this.total.forEach(function(x){
@@ -38,38 +42,29 @@ $(document).ready(function(){
 	var newPizza = new Pizza(size, price);
 	var newOrder = new Order();
 
-	console.log(newPizza);
-	
 	
 	
 	var selectedToppings = $('.checkbox :checked').map(function() {
 		return this.id;}).get();
-
-	console.log(selectedToppings);
 	
 	newPizza.toppings.forEach(function(topping){
 		for (var i = 0; i < newPizza.toppings.length; i++){
 			if (topping.name === selectedToppings[i]){
 			newOrder.total.push(topping.price);
 		}
-
 		}
-
-
 	});
 
 	
+	var finalPrice = newOrder.addPrice();
+	var orderTotal = newPizza.orderTotal(finalPrice);	
 	
-
-	console.log(newOrder.addPrice());
-	
-	
-
 	$("#show-result").show();
-	// $("#order").append()
-	
-	
-	
+	$("#order").append(orderTotal);
+	$("#order").append("<li> Size: " + newPizza.size + " $ " + newPizza.price + "</li>" +
+		"<li> Toppings: " + selectedToppings + "$ " + finalPrice + "</li>");
+
+		
 	});
 
 });

c3fa7d0b310f60a5e85e3a7e0ed0b20706f96470
Liam Ellis <liamhellis@gmail.com>
2016-04-08 11:42:43 -0700

    business, UI separation, near complete CSS styles, near complete bootstrap input foundations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+function Pizza(size){
+	this.size = size;
+	toppings = [];
+}
+
+function Order(){
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+//************************************ UI LOGIC ****************************//
+
 $(document).ready(function(){
 	
 
